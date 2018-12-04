ca82e58b61d993593f4f7704eef06deee28750d0
Elmer <alayelmer1993@gmail.com>
2018-12-03 21:07:27 -0600

    Creación de la clase nodo para la construcción del árbol ast

---
+++ src/com/compi2/usac/arbol/Nodo.java
@@ -0,0 +1,63 @@
+package com.compi2.usac.arbol;
+
+import java.util.ArrayList;
+
+/**
+ * @author Elmer Alay
+ * @version 1.0
+ * @since   03-12-2018
+ */
+public class Nodo {
+    private String etiqueta;
+    private ArrayList<Nodo> hijos = new ArrayList<>();
+    private String valor;
+    private int idNodo;
+    private String separador;
+    
+    public void addHijos(Nodo hijo){
+        hijos.add(hijo);
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+    public ArrayList<Nodo> getHijos() {
+        return hijos;
+    }
+
+    public void setHijos(ArrayList<Nodo> hijos) {
+        this.hijos = hijos;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public int getIdNodo() {
+        return idNodo;
+    }
+
+    public void setIdNodo(int idNodo) {
+        this.idNodo = idNodo;
+    }
+
+    public String getSeparador() {
+        return separador;
+    }
+
+    public void setSeparador(String separador) {
+        this.separador = separador;
+    }
+    
+    
+    
+}
